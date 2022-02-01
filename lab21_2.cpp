ea2555d8dae806c80124b2fd708d04f30d093e30
PoommipatWat <poommipat_wat@cmu.ac.th>
2022-02-01 19:13:58 +0700

    test

--- lab21_2.cpp
+++ lab21_2.cpp
@@ -6,7 +6,7 @@ struct Rect{
 };
 
 double overlap(Rect *R1, Rect *R2){
-	double p1 = R1 -> x, q1 = R1 -> y, x1 = R2 -> x, y1 = R2 -> y;
+	double p1 = R1 -> x, q1 = R1 -> y, x1 = R2 -> x, y1 = R2 -> y
 	double p2 = p1+R1 -> w, q2 = q1-R1 -> h;
 	double x2 = x1+R2 -> w, y2 = y1-R2 -> h;
 	if (!((p1 < x2) && (q1 > y2) && (p2 > x1) && (q2 < y1))) return 0;
